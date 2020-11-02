dfad282ebeccfede59c925560374204930f9203a
Samratkunwar <samratkunwar12@gmail.com>
2020-11-02 22:49:42 +1000

    folder cleanup

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -7,6 +7,7 @@ const images = {
     image4: "https://images.pexels.com/photos/597200/pexels-photo-597200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     image5: "https://images.pexels.com/photos/1589082/pexels-photo-1589082.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     image6: "https://images.pexels.com/photos/1700768/pexels-photo-1700768.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    image7: "https://images.pexels.com/photos/1927220/pexels-photo-1927220.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
 }
 
 const Home = () => {
@@ -22,25 +23,25 @@ const Home = () => {
                     <img src={images.image3} />
                     <img src={images.image5} />
                     <img src={images.image6} />
-                    <img src={images.image1} />
+                    <img src={images.image7} />
                 </div> 
                 <div className="column">
+                    <img src={images.image7} />
                     <img src={images.image2} />
+                    <img src={images.image3} />
+                    <img src={images.image4} />
+                    <img src={images.image6} />
+                    <img src={images.image1} />
+                    <img src={images.image5} />
+                </div> 
+                <div className="column">
+                    <img src={images.image1} />
+                    <img src={images.image2} />
+                    <img src={images.image7} />
                     <img src={images.image3} />
                     <img src={images.image4} />
                     <img src={images.image5} />
                     <img src={images.image6} />
-                    <img src={images.image1} />
-                    <img src={images.image1} />
-                </div> 
-                <div className="column">
-                    <img src={images.image1} />
-                    <img src={images.image2} />
-                    <img src={images.image3} />
-                    <img src={images.image4} />
-                    <img src={images.image5} />
-                    <img src={images.image6} />
-                    <img src={images.image1} />
                 </div> 
                
             </div>
